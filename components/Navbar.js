29e42b441d7af5c6913aa257380978562f92c6b8
acawholesale <wholesaleaca@gmail.com>
2026-03-20 02:52:57 +0100

    fix: navbar fixed au scroll sur mobile (sticky → fixed top-0)

--- components/Navbar.js
+++ components/Navbar.js
@@ -166,7 +166,7 @@ export default function Navbar() {
       </div>
 
       {/* Main Navbar */}
-      <nav className="bg-black sticky top-0 z-50 border-b border-white/10">
+      <nav className="bg-black fixed top-0 left-0 right-0 w-full z-50 border-b border-white/10">
         <div className="max-w-7xl mx-auto px-4">
           <div className="flex items-center justify-between h-14 md:h-16">
 
